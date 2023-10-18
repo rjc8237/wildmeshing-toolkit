602f12499453a43f1d44ebc1477316f9a66f9a88
Ryan Capouellez <rjc8237@nyu.edu>
2023-10-18 17:21:57 -0400

    Added tests for polygon topology initialization

--- tests/test_topology.cpp
+++ tests/test_topology.cpp
@@ -16,6 +16,66 @@
 
 using namespace wmtk;
 
+namespace {
+// Implicit opposite map for halfedges paired as [e] = {2*e, 2*e + 1}
+long opp(long h)
+{
+    return ((h % 2) == 0) ? (h + 1) : (h - 1);
+}
+} // namespace
+
+void check_polygon_mesh_edges(Eigen::Ref<const VectorXl> next, Eigen::Ref<const VectorXl> prev)
+{
+    REQUIRE(next.size() == prev.size());
+    long n_halfedges = next.size();
+
+    // Check that prev is a right and left inverse for next
+    for (long hi = 0; hi < n_halfedges; ++hi) {
+        CHECK(next[prev[hi]] == hi);
+        CHECK(prev[next[hi]] == hi);
+    }
+}
+
+void check_polygon_mesh_vertices(
+    Eigen::Ref<const VectorXl> prev,
+    Eigen::Ref<const VectorXl> to,
+    Eigen::Ref<const VectorXl> out)
+{
+    REQUIRE(prev.size() == to.size());
+    long n_halfedges = to.size();
+    long n_vertices = out.size();
+
+    // Check that to is invariant under circulation h -> prev[opp[h]]
+    for (long hi = 0; hi < n_halfedges; ++hi) {
+        CHECK(to[prev[opp(hi)]] == to[hi]);
+    }
+
+    // Check that out is a right inverse for h -> from[h] = to[opp[h]]
+    for (long vi = 0; vi < n_vertices; ++vi) {
+        CHECK(to[opp(out[vi])] == vi);
+    }
+}
+
+void check_polygon_mesh_faces(
+    Eigen::Ref<const VectorXl> next,
+    Eigen::Ref<const VectorXl> he2f,
+    Eigen::Ref<const VectorXl> f2he)
+{
+    REQUIRE(next.size() == he2f.size());
+    long n_halfedges = he2f.size();
+    long n_faces = f2he.size();
+
+    // Check that he2f is invariant under next
+    for (long hi = 0; hi < n_halfedges; ++hi) {
+        CHECK(he2f[next[hi]] == he2f[hi]);
+    }
+
+    // Check that f2he is a right inverse for he2f
+    for (long fi = 0; fi < n_faces; ++fi) {
+        CHECK(he2f[f2he[fi]] == fi);
+    }
+}
+
 TEST_CASE("topology_of_single_triangle", "[topology][2D]")
 {
     Eigen::Matrix<long, 1, 3> F;
@@ -172,6 +232,84 @@ TEST_CASE("topology_of_complex_meshes", "[topology][2D]")
         }
     }
 }
+
+TEST_CASE("topology_of_bubble", "[topology][polygon]")
+{
+    VectorXl next(2);
+    next << 1, 0;
+    std::vector<long> bnd_loops = {};
+    auto [prev, to, he2f, f2he, out] = polygon_mesh_topology_initialization(next, bnd_loops);
+
+    check_polygon_mesh_edges(next, prev);
+    check_polygon_mesh_vertices(prev, to, out);
+    check_polygon_mesh_faces(next, he2f, f2he);
+}
+
+TEST_CASE("topology_of_dual_bubble", "[topology][polygon]")
+{
+    VectorXl next(2);
+    next << 0, 1;
+    std::vector<long> bnd_loops = {};
+    auto [prev, to, he2f, f2he, out] = polygon_mesh_topology_initialization(next, bnd_loops);
+
+    check_polygon_mesh_edges(next, prev);
+    check_polygon_mesh_vertices(prev, to, out);
+    check_polygon_mesh_faces(next, he2f, f2he);
+}
+
+TEST_CASE("topology_of_single_triangle_polygon", "[topology][polygon]")
+{
+    Eigen::Matrix<long, 1, 3> F;
+    F << 0, 1, 2;
+    auto [next, prev, to, he2f, f2he, out, bnd_loops] = polygon_mesh_topology_initialization(F);
+
+    check_polygon_mesh_edges(next, prev);
+    check_polygon_mesh_vertices(prev, to, out);
+    check_polygon_mesh_faces(next, he2f, f2he);
+    CHECK(bnd_loops.size() == 1);
+}
+
+TEST_CASE("topology_of_two_triangle_polygons", "[topology][polygon]")
+{
+    Eigen::Matrix<long, 2, 3> F;
+    F << 0, 1, 2, 1, 3, 2;
+    auto [next, prev, to, he2f, f2he, out, bnd_loops] = polygon_mesh_topology_initialization(F);
+
+    check_polygon_mesh_edges(next, prev);
+    check_polygon_mesh_vertices(prev, to, out);
+    check_polygon_mesh_faces(next, he2f, f2he);
+    CHECK(bnd_loops.size() == 0);
+}
+
+TEST_CASE("topology_of_complex_polygon_meshes", "[topology][polygon]")
+{
+    Eigen::MatrixXd V;
+    Eigen::Matrix<long, -1, -1> F;
+
+    std::vector<std::string> names = {
+        "/Octocat.obj",
+        "/armadillo.obj",
+        "/blub.obj",
+        "/bunny.obj",
+        "/circle.obj",
+        "/fan.obj",
+        "/sphere.obj",
+        "/test_triwild.obj",
+        "/hemisphere.obj"};
+
+    for (auto name : names) {
+        std::string path;
+        path.append(WMTK_DATA_DIR);
+        path.append(name);
+        igl::read_triangle_mesh(path, V, F);
+        auto [next, prev, to, he2f, f2he, out, bnd_loops] = polygon_mesh_topology_initialization(F);
+
+        check_polygon_mesh_edges(next, prev);
+        check_polygon_mesh_vertices(prev, to, out);
+        check_polygon_mesh_faces(next, he2f, f2he);
+    }
+}
+
 
 TEST_CASE("topology_of_two_adjacent_tets", "[topology][3D]")
 {
